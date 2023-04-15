aeb09ae936e4faaf52e3d03a4119772dd00abbdb
sandhyakumari445 <85053570+sandhyakumari445@users.noreply.github.com>
2023-04-15 21:33:22 +0530

    "renamed"

--- setup.py
+++ setup.py
@@ -25,8 +25,8 @@ def get_requirements(file_path:str)->List[str]:
 setup(
     name = 'mlproject_regression',
     version= '0.0.1',
-    author='Utkarsh Gaikwad',
-    author_email='gaikwadujg@gmail.com',
+    author='Sandhya Kumari',
+    author_email='sandhyakumari445.sk@gmail.com',
     packages = find_packages(),
     install_requires = get_requirements('requirements.txt')
 )
